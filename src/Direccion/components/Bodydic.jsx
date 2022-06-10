54fccb4a7a199d4e12156d717ef3052207252423
AlejandroSugeno <Alexityco@gmail.com>
2022-06-10 09:39:47 -0400

    2alejandromontano

--- src/Direccion/components/Bodydic.jsx
+++ src/Direccion/components/Bodydic.jsx
@@ -6,7 +6,7 @@ function Bodydic() {
     return (
         <div className='cont-direc'>
             <span>Calle Chichas esquina Capitan Ravelo</span>
-            <span>A dos cuadras de la Plaza Isabela Catolica, ex "Katanas" #169</span>
+            <span>A dos cuadras de la Plaza Isabela Catolica #169</span>
             <img src={dic} alt="Ubicacion tienda" />
         </div>
     )
